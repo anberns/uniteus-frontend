1c69d2da15f30f69249c682a099bf317207e9fef
Aaron Berns <anberns@gmail.com>
2019-04-26 17:31:26 -0400

    handle server response by hiding form, displaying message

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -72,8 +72,30 @@ function Form(props) {
       })
     })
       .then(response => {
-        console.log(response.status)
+        handleResponse(response.status)
       })
+  }
+
+  const handleResponse = (status) => {
+    let messageDiv = document.getElementById("response-message")
+    let form = document.getElementById("form-div")
+    form.style.display = "none"
+    switch (status) {
+      case 201:
+        messageDiv.innerHTML = "Your assistance request has been successfully submitted."
+        break;
+      case 401:
+        messageDiv.innerHTML = "Sorry, you are not authorized to make this request."
+        break;
+      case 500:
+        messageDiv.innerHTML = "Oh no! Something completely unexpected happened!"
+        break;
+      case 503:
+        messageDiv.innerHTML = "We're down!!!!!! Come back later.....(please)"
+        break;
+      default:
+        break;
+    }
   }
 
   // create select options
@@ -86,21 +108,24 @@ function Form(props) {
   }
 
   return(
-    <div className="form-div">
-      <h1>New Assistance Request</h1>
-      <hr></hr>
-      <form className="needs-validation" onSubmit={handleSubmit} noValidate>
-        <input className="form-control" required type="text" value={firstName} onChange={handleFirstNameChange} placeholder="First Name"/><br></br>
-        <input className="form-control" required type="text" value={lastName} onChange={handleLastNameChange} placeholder="Last Name"/><br></br>
-        <input className="form-control" required type="text" value={email} onChange={handleEmailChange} placeholder="Email"/><br></br>
-        <select className="form-control" value={service} onChange={handleServiceChange}>
-          {serviceOptions}
-        </select><br></br>
-        <input className="form-control" type="textarea" value={comments} onChange={handleCommentsChange} /><br></br>
-        <input type="checkbox" checked={accept} onChange={handleAcceptChange}/>
-        <label>I hereby accept the terms of service for THE NETWORK and the Privacy Policy.</label><br></br>
-        <input className="btn btn-primary btn-sm" type="submit" value="Get Assistance"/>
-      </form>
+    <div className="container">
+      <div id="response-message"></div>
+      <div id="form-div">
+        <h1>New Assistance Request</h1>
+        <hr></hr>
+        <form id="request-form" className="needs-validation" onSubmit={handleSubmit} noValidate>
+          <input className="form-control" required type="text" value={firstName} onChange={handleFirstNameChange} placeholder="First Name"/><br></br>
+          <input className="form-control" required type="text" value={lastName} onChange={handleLastNameChange} placeholder="Last Name"/><br></br>
+          <input className="form-control" required type="text" value={email} onChange={handleEmailChange} placeholder="Email"/><br></br>
+          <select className="form-control" value={service} onChange={handleServiceChange}>
+            {serviceOptions}
+          </select><br></br>
+          <input className="form-control" type="textarea" value={comments} onChange={handleCommentsChange} /><br></br>
+          <input type="checkbox" checked={accept} onChange={handleAcceptChange}/>
+          <label>I hereby accept the terms of service for THE NETWORK and the Privacy Policy.</label><br></br>
+          <input className="btn btn-primary btn-sm" type="submit" value="Get Assistance"/>
+        </form>
+      </div>
     </div>
   );
 }
