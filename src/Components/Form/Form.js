549095c76299bf791c13d49c8c8d6103982a2eaa
Aaron Berns <anberns@gmail.com>
2019-04-26 12:17:37 -0400

    add conditional to prevent infinite re-rendering on setState call

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -2,17 +2,25 @@ import React, { useState, useEffect } from 'react'
 
 function Form(props) {
 
-  const [services, setServices] = useState();
+  const [services, setServices] = useState(null);
+
+  const fetchServices = () => {
+    if (services === null) {
+      fetch('http://localhost:49567/api/service-types')
+        .then(response => response.json())
+        .then(json => setServices(json.data))
+    }
+  }
 
   useEffect(() => {
-    fetch('localhost:49567/api/service-type')
-    .then(response => response.json)
-    .then(json => setServices(json))
+    fetchServices(); 
   });
+
+  console.log(services)
 
   return(
     <div>
-      {services}
+      hi
     </div>
   );
 }
