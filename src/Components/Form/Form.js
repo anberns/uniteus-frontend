073627e579b4307b538814380fafdf44a7ab40d2
Aaron Berns <anberns@gmail.com>
2019-04-27 07:27:43 -0400

    add basic input validation

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -8,6 +8,7 @@
   -test
 */
 import React, { useState, useEffect } from 'react'
+import './Form.css';
 
 function Form(props) {
 
@@ -64,26 +65,74 @@ function Form(props) {
   // post form data
   const handleSubmit = (event) => {
     event.preventDefault();
-    fetch('http://localhost:49567/api/assistance-requests', {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        "assistance_request": {
-          "contact": {
-            "first_name": firstName,
-            "last_name": lastName,
-            "email": email,
-          },
-          "service_type": service,
-          "description": description 
-          }
+
+    // validate fields
+    if (validateFields()) {
+      fetch('http://localhost:49567/api/assistance-requests', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          "assistance_request": {
+            "contact": {
+              "first_name": firstName,
+              "last_name": lastName,
+              "email": email,
+            },
+            "service_type": service,
+            "description": description 
+            }
+        })
       })
-    })
-    .then(response => {
-      handleResponse(response.status)
-    })
+      .then(response => {
+        handleResponse(response.status)
+      })
+    }
+  }
+
+  const validateFields = () => {
+
+    // reset visibility
+    const valDivs = document.getElementsByClassName("val-div");
+    for (const i of valDivs) {
+      i.style.visibility = 'hidden';
+    }
+
+    let allValid = true;
+
+    // firstName
+    let firstNameMessageDiv = document.getElementById("fname-val");
+    if (firstName === "") {
+      firstNameMessageDiv.style.visibility = "visible";
+      allValid = false;
+    }
+    // lastName
+    let lastNameMessageDiv = document.getElementById("lname-val");
+    if (lastName === "") {
+      lastNameMessageDiv.style.visibility = "visible";
+      allValid = false;
+    }
+    // email
+    let emailMessageDiv = document.getElementById("email-val");
+    if (email === "") {
+      emailMessageDiv.style.visibility = "visible";
+      allValid = false;
+    }
+    // service
+    let serviceMessageDiv = document.getElementById("service-val");
+    console.log(service)
+    if (service === undefined){
+      serviceMessageDiv.style.visibility = "visible";
+      allValid = false;
+    }
+    // checkbox 
+    let checkMessageDiv = document.getElementById("check-val");
+    if (accept === false) {
+      checkMessageDiv.style.visibility = "visible";
+      allValid = false;
+    }
+    return allValid;
   }
 
   // display server response
@@ -129,14 +178,19 @@ function Form(props) {
         <hr></hr>
         <form id="request-form" className="needs-validation" onSubmit={handleSubmit} noValidate>
           <input className="form-control" required type="text" name="fname" value={firstName} onChange={handleChange} placeholder="First Name"/><br></br>
+          <div className="val-div" id="fname-val">required</div>
           <input className="form-control" required type="text" name="lname" value={lastName} onChange={handleChange} placeholder="Last Name"/><br></br>
+          <div className="val-div" id="lname-val">required</div>
           <input className="form-control" required type="text" name="email" value={email} onChange={handleChange} placeholder="Email"/><br></br>
+          <div className="val-div" id="email-val">required</div>
           <select className="form-control" value={service} name="service" onChange={handleChange}>
             {serviceOptions}
           </select><br></br>
+          <div className="val-div" id="service-val">required</div>
           <input className="form-control" type="textarea" name="desc" value={description} onChange={handleChange} /><br></br>
           <input type="checkbox" checked={accept} name="check" onChange={handleChange}/>
           <label>I hereby accept the terms of service for THE NETWORK and the Privacy Policy.</label><br></br>
+          <div className="val-div" id="check-val">required</div>
           <input className="btn btn-primary btn-sm" type="submit" value="Get Assistance"/>
         </form>
       </div>
