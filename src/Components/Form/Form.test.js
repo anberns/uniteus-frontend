e439adc36dbf44178d9cde136fa0c7e814619300
Aaron Berns <anberns@gmail.com>
2019-04-27 20:54:44 -0400

    add tests for Form input requirements

--- src/Components/Form/Form.test.js
+++ src/Components/Form/Form.test.js
@@ -1,18 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme';
 import Form from './Form';
-
-// setup file
-import { configure } from 'enzyme';
+import TextInput from '../TextInput/TextInput'
+import { configure, shallow }from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-configure({ adapter: new Adapter() });
-describe('Form', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Form />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
+configure({adapter: new Adapter()});
 
-});
+describe('Form', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<Form />);
+    /*
+    wrapper.setProps({
+      inputClass: 'testClass',
+      type: 'text',
+      inputId: 'inputId',
+      inputName: 'inputName',
+      inputValue: 'inputValue',
+      onchange: testFn,
+      placeholder: 'placeholder',
+      valClass: 'valClass',
+      valId: 'valId'
+    })
+    */
+  })
+  it('renders a field for a first name', () => {
+    expect(wrapper.containsMatchingElement(<TextInput inputId="fname" type="text"/>)).toEqual(true);
+  }); 
+  it('renders a field for a last name', () => {
+    expect(wrapper.containsMatchingElement(<TextInput inputId="lname" type="text"/>)).toEqual(true);
+  });
+  it('renders a field for an email address', () => {
+    expect(wrapper.containsMatchingElement(<TextInput type="email"/>)).toEqual(true);
+  });
+  it('renders a select field for service type', () => {
+    expect(wrapper.find('select')).toHaveLength(1);
+  });
+  it('renders a textarea for a description', () => {
+    expect(wrapper.containsMatchingElement(<TextInput type="textarea"/>)).toEqual(true);
+  });
+  it('renders a checkbox for terms acknowledgement', () => {
+    expect(wrapper.containsMatchingElement(<input type="checkbox"/>)).toEqual(true);
+  });
+})
